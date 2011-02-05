e0a41e6258f6ea6f26a98ed26fbd8fd7da36f482
anmari <anmari@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-05 05:50:22 +0000

    version update
    git-svn-id: https://plugins.svn.wordpress.org/amr-ical-events-list/trunk@340912 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- amr-ical-events-list.php
+++ amr-ical-events-list.php
@@ -3,24 +3,34 @@
 Plugin Name: Events with ical files, by list or calendar (aka amr-ical-events-list)
 Author URI: http://anmari.com/
 Plugin URI: http://icalevents.anmari.com
-Version: 3.4
-Text Domain: amr-ical-events-list 
+Version: 3.7
+Text Domain: amr-ical-events-list
 Domain Path:  /lang
+
 Description: Display simple or highly customisable and styleable list of events.  Handles all types of recurring events, notes, journals, freebusy etc. Offers links to add events to viewers calendar or subscribe to whole calendar. Write Calendar Page</a>  and put [iCal http://yoururl.ics ] where you want the list of events.  To tweak: <a href="options-general.php?page=manage_amr_ical">Manage Settings Page</a>,  <a href="widgets.php">Manage Widget</a>.
-		
+
 /*  Copyright 2009  AmR iCal Events List  (email : anmari@anmari.com)
+
     This program is free software; you can redistribute it and/or modify
+
     it under the terms of the GNU General Public License as published by
+
     the Free Software Foundation; either version 2 of the License, or
+
     (at your option) any later version.
+
     This program is distributed in the hope that it will be useful,
+
     but WITHOUT ANY WARRANTY; without even the implied warranty of
+
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
     GNU General Public License see <http://www.gnu.org/licenses/>.
+
     for more details.
 */
 //  NB Change version in list main tooo define('AMR_ICAL_LIST_VERSION', '3.0.1');
-require_once('amr-ical-events-list-main.php');	
+require_once('amr-ical-events-list-main.php');
 require_once('amr-ical-config.php');
 require_once('amr-ical-list-admin.php');
 require_once('amr-import-ical.php');
@@ -29,16 +39,20 @@ require_once('amr-ical-uninstall.php');
 require_once('amr-upcoming-events-widget.php');
 require_once('amr_date_i18n.php');
 require_once('amr-ical-calendar.php');
-define( 'AMR_BASENAME', plugin_basename( __FILE__ ) );	
-add_filter('plugin_action_links', 'amr_plugin_action', 8, 2);	
+define( 'AMR_BASENAME', plugin_basename( __FILE__ ) );
+add_filter('plugin_action_links', 'amr_plugin_action', 8, 2);
+
 function amr_plugin_action($links, $file) {
 	/* create link */
 	if ( $file == AMR_BASENAME ) {
-		if (function_exists('amr_events_settings_menu')) 
+		if (function_exists('amr_events_settings_menu'))
+
 		array_unshift($links,'<a href="admin.php?page=manage_amr_ical">'. __('Settings','amr_ical_list_lang').'</a>' );
-		else 
+		else
 		array_unshift($links,'<a href="options-general.php?page=manage_amr_ical">'. __('Settings','amr_ical_list_lang').'</a>' );
+
 	}
+
 	return $links;
 	} // end plugin_action()
 ?>
